e6d02008ded08b7b5c8a7747470a9a4c9011d57d
Yunus96 <mohammadyunus102@gmail.com>
2026-09-02 15:06:15 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { AuthProvider } from '@propelauth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <AuthProvider authUrl="https://72772615727.propelauthtest.com">
+            <Component {...pageProps} />
+        </AuthProvider>
+    )
 }
